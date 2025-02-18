49a1e14412ee3bd473506b5ff8d6e308e35ce1cf
notepgj <pranveragjonbalaj0@gmail.com>
2025-02-18 20:48:37 +0100

    I added another print. I added input

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 print("first commit")
-
+print("Pranvera")
+input("What's your name?")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
